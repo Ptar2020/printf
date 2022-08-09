bae65c21cd4be55d00ecb1dc8a184aeb803a0ea2
Ptar* <62691985+Ptar2020@users.noreply.github.com>
2022-08-09 23:20:22 +0300

    Update answers

--- printf.c
+++ printf.c
@@ -2,23 +2,35 @@
 #include "main.h"
 #include <stdarg.h>
 /**
- *
- *
+ * _printf - the printing function
+ * @format: The string character
+ * Return: The printed character
  */
 
 int _printf(const char *format, ...)
 {
+	st_fmt st_format[] =
+ {
+		{"c", func_char},
+		{"s", func_string},
+		{"d", func_digit},
+		{"i", func_digit},
+		{"%", func_percent},
+		{"b", func_binary_convert},
+		{"u", func_unsig_int},
+		{"o", func_octal_convert},
+		{"x", func_hex_Lowcase_convert},
+		{"X", func_hex_Upcase_convert},
+		{"S", func_stringUppercase},
+		{"r", func_revstr},
+		{"R", func_rot13},
+		{NULL, NULL}
+};
 	va_list args;
-	unsigned int i;
-	char *items[];
+	int counter;
 
 	va_start(args, format);
-		
-	for (i = 0; i < format; i++)
-	{
-		items = va_arg(format, char *);
-
-	}
+	counter = get_funct(format, args, st_format);
 	va_end(args);
-	return items;
+	return counter;
 }
